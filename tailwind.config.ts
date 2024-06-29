fee4f89cff2f6c86b6df8a7483115fce1b0a54ce
Abdullah Sharifi <ab.sharifi19@gmail.com>
2024-06-29 20:29:35 +0200

    Define the css colors and remove extra content

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,10 +9,23 @@ const config: Config = {
   theme: {
     extend: {
       backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+        "bt-bg-gradient": "linear-gradient(to right, #FF9C1A, #E80505)"
       },
+      colors : {
+        "primary-font-color": "black",
+        "secondary-font-color": "black/50",
+        "primary-bg-color": "#F2F7FC",
+        "secondary-bg-color": "#FFEBD1",
+        "skill-bg-color": "#E1E8EF",
+        "social-color": "#1DA1F2",
+        "icon-color": "#FF9C1B",
+        // Dark mode colors
+        "dark-primary-font-color": "#F0F0F0",
+        "dark-secondary-font-color": "#A0A0A0",  
+        "dark-primary-bg-color": "#121212", 
+        "dark-secondary-bg-color": "#1E1E1E", 
+        "dark-skill-bg-color": "#2A2A2A",
+      },    
     },
   },
   plugins: [],
