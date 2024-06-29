4bd786f3cc77f097c366cda9515cdcd81816cb28
Abdullah Sharifi <ab.sharifi19@gmail.com>
2024-06-29 20:40:21 +0200

    Create font.ts to import the necessary fonts

---
+++ src/fonts.ts
@@ -0,0 +1,4 @@
+import { Poppins, Raleway } from 'next/font/google';
+
+export const poppins = Poppins({ subsets: ["latin"], weight: ['400', '500', '600', '700'], });
+export const raleway = Raleway({ subsets: ["latin"] });
