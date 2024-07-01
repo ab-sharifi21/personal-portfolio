722729f50e1d71b6f9b5293fccb28ce0bb8dbd27
Abdullah Sharifi <ab.sharifi19@gmail.com>
2024-07-01 18:48:22 +0200

    Add arrow and smiley face svg in home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,26 +3,37 @@ import Image from 'next/image';
 
 export default function Home() {
   return (
-    <div className="w-full flex-1 rounded-2xl bg-white/50 p-4 pl-8 pr-8 shadow-box-shadow dark:bg-dark-skill-bg-color desktop:pl-12 desktop:pr-12">
-      <h2
-        className={`${poppins.className} mb-4 text-[1.3rem] font-bold capitalize`}
-      >
-        About me<span className="text-2xl text-icon-color">.</span>
-      </h2>
-      <div className="flex justify-between">
-        <p className="max-w-xl">
-          I am a highly motivated web developer with a range of skills spanning
-          from front-end to back-end. I would like to join a dynamic team, one
-          that not only allows me to utilize my existing skills but also offers
-          an environment where I can grow both technically and professionally.
-        </p>
-        <Image
-          src="/working.svg"
-          width={280}
-          height={200}
-          alt="Developer svg"
-          className="ml-8 mt-[-2rem] h-[180px] w-[280px]"
-        />
+    <div className="w-full flex-1 rounded-2xl bg-white/50 p-6 shadow-box-shadow dark:bg-dark-skill-bg-color desktop:pl-12 desktop:pr-12">
+      <div className='flex gap-4 justify-between'>
+        <div>
+          <h2
+            className={`${poppins.className} mb-4 text-[1.3rem] font-bold capitalize`}
+          >
+            About me<span className="text-2xl text-icon-color">.</span>
+          </h2>
+            <p className="max-w-md desktop:max-w-xl bg-secondary-bg-color desktop:p-4 p-3 pt-4 pb-4 rounded-md shadow-box-shadow dark:bg-cards-bg dark:shadow-2xl">
+              I am a highly motivated <span className='text-icon-color'>full-stack web developer</span> with a range of skills spanning
+              from front-end to back-end. I would like to join a dynamic team, one
+              that not only allows me to utilize my existing skills but also offers
+              an environment where I can grow both technically and professionally.
+            </p>
+        </div>
+        <div className='hidden desktop:block'>
+          <Image src='/arrow.svg' alt='right arrow' width={250} height={100} className='mt-24' />
+        </div>
+        <div>
+          <div className='flex items-center gap-3'>
+            <h2
+              className={`${poppins.className} mb-4 text-[1.3rem] font-bold capitalize`}
+            >
+              Something interesting<span className="text-2xl text-icon-color">.</span>
+            </h2>
+            <Image src='/smile.svg' alt='smiley face' width={45} height={45} className='cursor-pointer duration-500 hover:scale-125 hover:rotate-20 max-w-[45px] max-h-[45px]' />
+          </div>
+            <p className="max-w-sm desktop:max-w-md bg-bg-color3 p-2 desktop:pr-4 desktop:pl-4 rounded-md shadow-box-shadow dark:bg-cards-bg dark:shadow-2xl">
+              <span className='text-icon-color'>When I first saw a computer I was 12 years old</span>. And I have been curious about technology and video games ever since. So i Jumped into this amazing adventure, exploring and discovering everything about them all on my own.
+            </p>
+        </div>
       </div>
     </div>
   );
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -16,12 +16,14 @@ const config: Config = {
       },
       backgroundImage: {
         'bt-bg-gradient': 'linear-gradient(to right, #FF9C1A, #E80505)',
+        'cards-bg': 'radial-gradient(circle 160px at 50% 120%, #353535, #1f1f1f)',
       },
       colors: {
         'primary-font-color': 'black',
         'secondary-font-color': 'black/50',
         'primary-bg-color': '#F2F7FC',
-        'secondary-bg-color': '#FFEBD1',
+        'secondary-bg-color': '#ffe3bf',
+        'bg-color3': '#ffeed9',
         'skill-bg-color': '#E1E8EF',
         'social-color': '#1DA1F2',
         'icon-color': '#FF9C1B',
@@ -29,7 +31,7 @@ const config: Config = {
         'dark-primary-font-color': '#F0F0F0',
         'dark-secondary-font-color': '#A0A0A0',
         'dark-primary-bg-color': '#333333',
-        'dark-secondary-bg-color': '#1E1E1E',
+        'dark-secondary-bg-color': '#212121',
         'dark-skill-bg-color': '#2A2A2A',
       },
       boxShadow: {
@@ -37,6 +39,7 @@ const config: Config = {
       },
       rotate: {
         '360': '360deg',
+        '20': '20deg'
       },
     },
   },
