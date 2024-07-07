532b32164118513a256d89726a217f036fa666cb
Abdullah Sharifi <ab.sharifi19@gmail.com>
2024-07-07 21:26:04 +0200

    Change the title of the contact page

--- README.md
+++ README.md
@@ -1,5 +1,14 @@
-# New web developer portfolio website with Next.js, Tailwindcss and TypeScript
+# New web developer portfolio website with Next.js
 
 ## The design
 
 [figma design](<https://www.figma.com/design/JnimM2vOsAs5zwpFFR2lvx/Portfolio-Design-(Community)?node-id=0-1&t=1sv2mEKyEFgcweA1-0>)
+
+# Technologies
+
+1. HTML
+2. Tailwind CSS
+3. Next.js
+4. TypeScript
+5. react-icons
+6. next-themes
--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -4,7 +4,7 @@ import { MdOutlinePermContactCalendar } from 'react-icons/md';
 export default function Home() {
   return (
     <div className="w-full flex-1 rounded-2xl bg-white/50 p-4 pl-8 pr-8 shadow-box-shadow dark:bg-dark-skill-bg-color">
-      <div className="mb-6 flex items-center gap-2">
+      <div className="flex items-center gap-2">
         <MdOutlinePermContactCalendar className="h-6 w-6 text-icon-color" />
         <h2
           className={`${poppins.className} text-[1.3rem] font-bold capitalize`}
@@ -12,6 +12,9 @@ export default function Home() {
           Contact<span className="text-2xl text-icon-color">.</span>
         </h2>
       </div>
+      <p className="mb-8 text-sm text-primary-font-color/50 dark:text-dark-primary-font-color/50">
+        Get in touch and let&apos;s work together!
+      </p>
     </div>
   );
 }
