90a0cd3278057e4db79ebd547fffe85c03bdbd82
Abdullah Sharifi <ab.sharifi19@gmail.com>
2024-07-15 20:18:18 +0200

    Adjust side bar styles for mobile and desktop designs

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -40,7 +40,7 @@ export const SideBar = () => {
   ];
 
   return (
-    <aside className="relative mt-[5rem] flex w-[85%] flex-col items-center rounded-2xl bg-white/50 p-[1.5rem] pb-[3rem] pt-[8rem] shadow-box-shadow dark:bg-dark-skill-bg-color mobile:w-[60%] tablet:max-w-[280px]">
+    <aside className="relative mt-[5rem] flex w-[85%] flex-col items-center rounded-2xl bg-white/50 p-[1.5rem] pb-[2rem] pt-[8rem] shadow-box-shadow dark:bg-dark-skill-bg-color mobile:w-[60%] tablet:sticky tablet:top-[7rem] tablet:max-w-[280px]">
       <Image
         src="/me.jpg"
         alt="Abdullah photo"
@@ -55,27 +55,19 @@ export const SideBar = () => {
       </h1>
       <p className="font-semibold text-icon-color">Full-Stack Web Developer</p>
       <SocialMediaList />
+      <div className="hidden w-full flex-1 rounded-2xl bg-primary-bg-color p-[1rem] py-6 shadow-box-shadow dark:bg-dark-secondary-bg-color dark:shadow-2xl tablet:block">
+        {contacts.map((contact) => {
+          return <ContactInfoCard key={contact.id} contact={contact} />;
+        })}
+      </div>
       <a
         download
         href="/abdullah-sharifi.pdf"
-        className={`${poppins.className} flex items-center justify-center gap-2 rounded-full bg-bt-bg-gradient p-3 pl-4 pr-4 font-semibold text-white/90 duration-300 hover:scale-105 tablet:hidden`}
+        className={`${poppins.className} flex items-center justify-center gap-2 rounded-full bg-bt-bg-gradient p-3 pl-4 pr-4 font-semibold text-white/90 duration-300 hover:scale-105 tablet:mt-8`}
       >
         <MdOutlineDownload className="h-5 w-5" />
         <span>Download CV</span>
       </a>
-      <div className="hidden w-full flex-1 rounded-2xl bg-primary-bg-color p-[1rem] pb-8 pt-8 shadow-box-shadow dark:bg-dark-secondary-bg-color dark:shadow-2xl tablet:block">
-        {contacts.map((contact) => {
-          return <ContactInfoCard key={contact.id} contact={contact} />;
-        })}
-        <a
-          download
-          href="/abdullah-sharifi.pdf"
-          className={`${poppins.className} mt-8 flex items-center justify-center gap-2 rounded-full bg-bt-bg-gradient p-3 pl-4 pr-4 font-semibold text-white/90 duration-300 hover:scale-105`}
-        >
-          <MdOutlineDownload className="h-5 w-5" />
-          <span>Download CV</span>
-        </a>
-      </div>
     </aside>
   );
 };
