35bc684835f934bb99346faf3858ffa09bcc22f0
Abdullah Sharifi <ab.sharifi19@gmail.com>
2024-11-24 18:27:31 +0100

    Create LocaleSwitcher component and use it in Header component

---
+++ src/components/LocaleSwitcher.tsx
@@ -0,0 +1,23 @@
+import { useLocale, useTranslations } from 'next-intl';
+import LocaleSwitcherSelect from './LocaleSwitcherSelect';
+
+export default function LocaleSwitcher() {
+    const t = useTranslations('LocaleSwitcher');
+    const locale = useLocale();
+
+    return (
+        <LocaleSwitcherSelect
+            defaultValue={locale}
+            items={[
+                {
+                    value: 'en',
+                    label: t('en'),
+                },
+                {
+                    value: 'es',
+                    label: t('es'),
+                },
+            ]}
+        />
+    );
+}
