336abd38b815a08693a591637702248b96cd72b7
Abdullah Sharifi <ab.sharifi19@gmail.com>
2024-11-26 18:07:06 +0100

    Add contact page's translations

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,11 +1,13 @@
 import { ContactForm } from '@/components';
 import { poppins } from '@/fonts';
+import { useTranslations } from 'next-intl';
 import { MdOutlinePermContactCalendar } from 'react-icons/md';
 
 export default function Contact() {
   const userId = process.env.USER_ID;
   const serviceId = process.env.SERVICE_ID;
   const templateId = process.env.TEMPLATE_ID;
+  const t = useTranslations();
   return (
     <div className="flex w-full flex-1 flex-col rounded-2xl bg-white/50 p-4 pl-8 pr-8 shadow-box-shadow dark:bg-dark-skill-bg-color">
       <div className="flex items-center gap-2">
@@ -13,11 +15,11 @@ export default function Contact() {
         <h2
           className={`${poppins.className} text-[1.3rem] font-bold capitalize`}
         >
-          Contact<span className="text-2xl text-icon-color">.</span>
+          {t('contact_page.title')}<span className="text-2xl text-icon-color">.</span>
         </h2>
       </div>
       <p className="mb-8 text-sm dark:text-dark-primary-font-color/50">
-        Get in touch and let&apos;s work together!
+        {t('contact_page.contact_message')}
       </p>
 
       <ContactForm
